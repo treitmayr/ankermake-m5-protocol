6131f6beca08263c61f477a893842b2d55ba5de1
Eric Lin <anselor@gmail.com>
2024-03-17 09:38:37 -0400

    Changed ankerctl to read from .env or .flaskenv on startup. Added pyinstaller packaging for windows users. Updated requirements needed to run transwarp.

--- ankerctl.py
+++ ankerctl.py
@@ -29,6 +29,11 @@ from libflagship.pppp import P2PCmdType, P2PSubCmdType, FileTransfer
 from libflagship.ppppapi import FileUploadInfo, PPPPError
 
 
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env.
+
+
 class Environment:
     def __init__(self):
         pass
